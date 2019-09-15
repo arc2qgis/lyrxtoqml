e9080ba8fceb9e290085087535a2f19cd4eb9590
bogind <dror.bogin@gmail.com>
2019-09-15 14:28:15 +0300

    ui set to minimal expanding

--- plugin/qlyrx/resources.py
+++ plugin/qlyrx/resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.9.4)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.9.6)
 #
 # WARNING! All changes made in this file will be lost!
 
